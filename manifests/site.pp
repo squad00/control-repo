241ea021b4cebc32d9e4f86b6daf603a403d691d
Syed Quadri <syedquadri00@gmail.com>
2021-03-28 22:54:44 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,9 @@
 node puppet.local {
   include role::master
 }
+node 'puppetserver.example.com' {
+  notify { 'this is the puppet master': }
+}
+node default {
+  notify { 'this node did not match any of the listed definitions': }
+}
