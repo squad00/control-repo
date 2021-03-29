c178227abd967710ecdba5335ae498dc01c865a7
Syed Quadri <syedquadri00@gmail.com>
2021-03-28 23:13:23 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,5 @@
-node puppet.local {
-  include role::master
-}
 node 'ip-10-0-1-47.ec2.internal' {
+  include role::master
   notify { 'this is the puppet master': }
-}
-node default {
-  notify { 'this node did not match any of the listed definitions': }
+
 }
