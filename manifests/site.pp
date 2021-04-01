741f1fc1b764eaaf4227c1f33dc06ab8d38dd9fc
Syed Quadri <squadri@conversantmedia.com>
2021-03-31 19:52:05 -0500

    file resource add

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,7 @@
 node 'ip-10-0-1-47.ec2.internal' {
   include role::master
-  notify { 'this is the puppet master': }
-
+  file {'/etc/secret_password.txt':
+    ensure => file,
+    content => lookup('secret_password'),
+  }
 }
